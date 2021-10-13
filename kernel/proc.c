d412f32bd266eeeabc43845cd715a79f9f84922d
sleepCOW <olexthelake@gmail.com>
2021-10-13 18:45:19 +0300

    System call tracing, Sysinfo

--- kernel/proc.c
+++ kernel/proc.c
@@ -295,6 +295,8 @@ fork(void)
 
   np->state = RUNNABLE;
 
+  np->trace_mask = p->trace_mask;
+
   release(&np->lock);
 
   return pid;
@@ -692,4 +694,17 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+uint64
+get_nproc(void) {
+
+  uint64 nproc = 0;
+  struct proc *p;
+
+  for(p = proc; p < &proc[NPROC]; p++) {
+    nproc += p->state != UNUSED;
+  }
+
+  return nproc;
 }
